9f80d8dc315dd17b56b7d2c6c59a5a1d037f2db2
adnan <adshe886@gmail.com>
2023-08-24 21:51:44 +0530

    changes

--- backend/uploadsModel.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.Schema = new mongoose.Schema
--- backend/validators/mongoIdValidator.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const areMongoIdsValid = async (ids) => {
-    if (!Array.isArray(ids) || !ids.length) {
-        return { valid: false, message: 'ids should be in array and valid' }
-    }
-    return ids.every(id => mongoose.Types.ObjectId.isValid(id))
-}
-
-module.exports = areMongoIdsValid;

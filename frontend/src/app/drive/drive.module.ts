533fedf3f04a75924fd5cd20972dce787622cb48
adnan <adshe886@gmail.com>
2023-08-30 00:09:26 +0530

    drive component

--- frontend/src/app/drive/drive.module.ts
+++ frontend/src/app/drive/drive.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DriveRoutingModule } from './drive-routing.module';
-import { DriveComponent } from './drive.component';
+import { DriveComponent } from './components/drive-component/drive.component';
 
 
 @NgModule({
